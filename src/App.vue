640487510f6d590623f1ea37cb600acb61c14101
hungernice <329407244@qq.com>
2018-02-23 23:43:21 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+		<Header></Header>
+		<div class="container-fluid">
+			<div class="row">				
+				<LeftNav></LeftNav>
+				<Content></Content>
+			</div>
+		</div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import LeftNav from './components/LeftNav.vue'
+import Content from './components/Content.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  components:{
+  	Header,
+  	LeftNav,
+  	Content
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/LeftNav.vue
@@ -0,0 +1,55 @@
+<template>
+	<!--<div id="left">-->
+		<div class="col-sm-3 col-md-2 sidebar">
+			<ul class="nav nav-sidebar">
+				<li class="active">
+					<a href="#">导航一</a>
+				</li>
+				<li>
+					<a href="#">Reports</a>
+				</li>
+				<li>
+					<a href="#">Analytics</a>
+				</li>
+				<li>
+					<a href="#">Export</a>
+				</li>
+			</ul>
+			<ul class="nav nav-sidebar">
+				<li>
+					<a href="">导航二</a>
+				</li>
+				<li>
+					<a href="">Nav item again</a>
+				</li>
+				<li>
+					<a href="">One more nav</a>
+				</li>
+				<li>
+					<a href="">Another nav item</a>
+				</li>
+				<li>
+					<a href="">More navigation</a>
+				</li>
+			</ul>
+			<ul class="nav nav-sidebar">
+				<li>
+					<a href="">导航三</a>
+				</li>
+				<li>
+					<a href="">One more nav</a>
+				</li>
+				<li>
+					<a href="">Another nav item</a>
+				</li>
+			</ul>
+		</div>		
+		
+	<!--</div>-->
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
